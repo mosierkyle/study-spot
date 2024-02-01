7b53a6e146b7be14923c775832254368b93a065b
Kyle Mosier <mosierkyle@gmail.com>
2024-02-01 12:11:47 -0800

    Configure next auth google Oauth sign in

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,14 @@
-const About = () => {
+// import { School } from '@prisma/client';
+import { prisma } from '../../lib/prisma';
+
+const About = async () => {
+  //   const feed = await prisma.school.findMany();
   return (
     <div>
       <h1>About Page</h1>
+      {/* {feed.map((school: School) => {
+        return <p key={school.id}>{school.name}</p>;
+      })} */}
     </div>
   );
 };
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma: PrismaClient = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.POSTGRES_PRISMA_URL, // Use process.env to access environment variables
-    },
-  },
-});
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export default prisma;
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
