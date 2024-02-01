0d9309d22cb01338c8ec955829b8fab01e7e23b3
Kyle Mosier <mosierkyle@gmail.com>
2024-02-01 12:11:47 -0800

    Configure next auth google Oauth sign in

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import Link from 'next/link';
+import { getUserSession } from '@/lib/session';
 
-export default function Home() {
-  return <main className={styles.main}>Hey</main>;
+export default async function Home() {
+  const user = await getUserSession();
+  return (
+    <main className={styles.main}>
+      <h1>This is the home page</h1>
+      <p>{JSON.stringify(user)}</p>
+      <Link href="api/auth/signin">Sign In</Link>
+      <Link href="about">Link to About Page</Link>
+    </main>
+  );
 }
