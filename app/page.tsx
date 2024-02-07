ccdf69a5476e699e9a2c93229ec6646760445495
Kyle Mosier <mosierkyle@gmail.com>
2024-02-07 12:55:02 -0800

    Create and style hero

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,37 @@ import Link from 'next/link';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import { User } from './user';
+import Image from 'next/image';
+import school1 from '../public/school1.jpg';
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
   // const user = await getUserSession();
   return (
     <main className={styles.main}>
-      <h1>This is the home page</h1>
-      {/* <p>{JSON.stringify(user)}</p> */}
-      <Link href="api/auth/signin">Sign In</Link>
-      <Link href="about">Link to About Page</Link>
-      <h2>Server Session</h2>
-      <pre>{JSON.stringify(session)}</pre>
-      <h2>Client Call</h2>
-      <User />
+      <div className={styles.hero}>
+        <div className={styles.heroImgDiv}>
+          <Image
+            className={styles.heroImg}
+            alt="University photo"
+            src={school1}
+          />
+        </div>
+        <h1 className={styles.heroText}>Find a better place to study</h1>
+        <div className={styles.searchDiv}>
+          <input
+            className={styles.search}
+            type="text"
+            placeholder="Search for your school"
+          />
+        </div>
+        <Link href="api/auth/signin">Sign In</Link>
+        <Link href="about">Link to About Page</Link>
+        <h2>Server Session</h2>
+        <pre>{JSON.stringify(session)}</pre>
+        <h2>Client Call</h2>
+        <User />
+      </div>
     </main>
   );
 }
