1f411a6cbf9ff612f985d2a5c0d0ccce6784c1cd
Kyle Mosier <mosierkyle@gmail.com>
2024-02-07 16:33:31 -0800

    Add a footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,10 @@ import './globals.css';
 import { Providers } from './providers';
 import styles from './page.module.css';
 import Link from 'next/link';
+import github2 from '../public/github2.png';
+import linkedin2 from '../public/linkedin2.png';
+import Image from 'next/image';
+import twitter from '../public/twitter.png';
 
 export const metadata: Metadata = {
   title: 'StudySpot',
@@ -37,6 +41,53 @@ export default function RootLayout({
         </div>
         <div>
           <Providers>{children}</Providers>
+        </div>
+        <div className={styles.footer}>
+          <div className={styles.logoSocial}>
+            <Link className={styles.footerLogo} href={'/'}>
+              StudySpot
+            </Link>
+            <Link
+              className={styles.footerSocial}
+              href={'https://github.com/mosierkyle'}
+            >
+              <Image height={30} width={30} alt="Github" src={github2} />
+            </Link>
+            <Link
+              className={styles.footerSocial}
+              href={'https://www.linkedin.com/in/kylemosier/'}
+            >
+              <Image height={30} width={30} alt="LinkedIn" src={linkedin2} />
+            </Link>
+            <Link
+              className={styles.footerSocial}
+              href={'https://github.com/mosierkyle'}
+            >
+              <Image height={30} width={30} alt="Twitter" src={twitter} />
+            </Link>
+          </div>
+          <div className={styles.footerLinks}>
+            <Link className={styles.footerLink1} href={'/'}>
+              About
+            </Link>
+            <Link className={styles.footerLink1} href={'/'}>
+              Account
+            </Link>
+            <Link className={styles.footerLink1} href={'/'}>
+              Contact
+            </Link>
+          </div>
+          <div className={styles.footerHelp}>
+            <Link className={styles.footerLink2} href={'/'}>
+              Terms & Conditions
+            </Link>
+            <Link className={styles.footerLink2} href={'/'}>
+              Privacy Policy
+            </Link>
+            <Link className={styles.footerLink2} href={'/'}>
+              All Rights Reserved
+            </Link>
+          </div>
         </div>
       </body>
     </html>
