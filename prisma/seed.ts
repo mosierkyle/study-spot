0f727aa8a2c36df1638fa3fbc4bcc8944b744bb0
Kyle Mosier <mosierkyle@gmail.com>
2024-02-19 17:03:51 -0800

    Add getLatLong functionality with mapbox api

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,11 +3,82 @@ import { hash } from 'bcrypt';
 
 async function main() {
   const school = await prisma.school.upsert({
-    where: { name: 'Cal Poly SLO' },
+    where: { name: 'Cal Poly San Luis Obispo' },
     update: {},
     create: {
-      name: 'Cal Poly SLO',
+      name: 'Cal Poly San Luis Obispo',
+      city: 'San Luis Obispo',
       address: '1 Grand Ave San Luis Obispo, CA 93407-005',
+    },
+  });
+
+  await prisma.school.upsert({
+    where: { name: 'Univeristy of Washington' },
+    update: {},
+    create: {
+      name: 'Univeristy of Washington',
+      city: 'Seattle',
+      address: '1410 NE Campus Pkwy, Seattle, WA 98195',
+    },
+  });
+
+  await prisma.school.upsert({
+    where: { name: 'Univeristy of Washington' },
+    update: {},
+    create: {
+      name: 'Univeristy of Washington',
+      city: 'Pullman',
+      address: '1500 Glenn Terrell Mall Pullman, WA  99163 United States',
+    },
+  });
+
+  await prisma.school.upsert({
+    where: { name: 'Univeristy of Arizona' },
+    update: {},
+    create: {
+      name: 'Univeristy of Arizona',
+      city: 'Tucson',
+      address: '1200 E University Blvd, Tucson, AZ 85721',
+    },
+  });
+
+  await prisma.school.upsert({
+    where: { name: 'Boise State University' },
+    update: {},
+    create: {
+      name: 'Boise State University',
+      city: 'Boise',
+      address: '1910 W University Dr, Boise, ID 83725',
+    },
+  });
+
+  await prisma.school.upsert({
+    where: { name: 'UCLA' },
+    update: {},
+    create: {
+      name: 'UCLA',
+      city: 'Los Angeles',
+      address: '617 Charles E Young Dr S, Los Angeles, CA 90095',
+    },
+  });
+
+  await prisma.school.upsert({
+    where: { name: 'Oregon State University' },
+    update: {},
+    create: {
+      name: 'Oregon State University',
+      city: 'Corvallis',
+      address: '1500 SW Jefferson Way, Corvallis, OR 97331',
+    },
+  });
+
+  await prisma.school.upsert({
+    where: { name: 'San Diego State University' },
+    update: {},
+    create: {
+      name: 'San Diego State University',
+      city: 'San Diego',
+      address: '5500 Campanile Dr, San Diego, CA 92182',
     },
   });
 
@@ -33,19 +104,18 @@ async function main() {
     create: {
       name: 'Kennedy Library',
       address: '1 Grand Ave Building 35, San Luis Obispo, CA 93401',
+      description:
+        'This is the primary library on campus, it has 5 floors and a 24 hub.',
       schoolId,
       userId,
       latitude: '35.293289',
       longitude: '-120.653152',
-      freeWifi: 'If you have a cal poly email yes',
+      wifi: 'If you have a cal poly email yes',
       noiseLevel: '5th floor is quietest, 1st is loudest',
       hours: 'Hub 24 is 24 hours with a poly card',
-      seatingCapacity: 'lots',
-      publicRestrooms: 'yes',
-      powerOutlets: 'yes',
-      accessibility: 'good',
-      indoorOutdoor: 'both',
-      studyResources: 'printers, whiteboards, chargers, etc.',
+      seating: 'lots',
+      restrooms: 'yes',
+      studyResources: ['printers', 'whiteboards', 'chargers', 'etc.'],
     },
     include: { school: true },
   });
