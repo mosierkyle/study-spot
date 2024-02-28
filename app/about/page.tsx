2329afcd0185309d9181acd672780e1c72dea347
Kyle Mosier <mosierkyle@gmail.com>
2024-02-28 13:38:38 -0800

    Rating stars component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -20,51 +20,12 @@ interface Props {
     school: string;
   };
 }
+import Stars from '../components/stars/stars';
 
 const About = () => {
-  // const [viewState, setViewState] = useState({
-  //   latitude: 35.305,
-  //   longitude: -120.6625,
-  //   zoom: 14.5,
-  //   pitch: 0,
-  // });
-
-  // const location = await getLatLong('2134 Santa Ynez Ave San Luis Obispo');
-  // const map = getMap();
-  const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
-  const mapContainer = useRef<any>(null);
-  const map = useRef<mapboxgl.Map | null>(null);
-  const [lng, setLng] = useState(-120.6625);
-  const [lat, setLat] = useState(35.305);
-  const [zoom, setZoom] = useState(12);
-
-  useEffect(() => {
-    mapboxgl.accessToken = mapboxToken ?? '';
-    if (!mapContainer.current || map.current) {
-      return;
-    }
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v12',
-      center: [lng, lat],
-      zoom: zoom,
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <main className={styles.mainStyle}>
-      {/* <Map
-        {...viewState}
-        onMove={(evt) => setViewState(evt.viewState)}
-        mapboxAccessToken={mapboxToken}
-        mapStyle="mapbox://styles/mapbox/light-v8"
-        style={{ height: 400 }}
-        maxZoom={15}
-        minZoom={10}
-        reuseMaps
-      ></Map> */}
-      <div className={styles.mapContainer} ref={mapContainer}></div>
+      <Stars rating={4.5} />
     </main>
   );
 };
