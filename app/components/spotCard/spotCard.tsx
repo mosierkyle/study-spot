557babf38991b5538d6f03144bafd9de5afd4374
Kyle Mosier <mosierkyle@gmail.com>
2024-03-05 11:37:46 -0800

    Load reviews in SpotCard Component

--- app/components/spotCard/spotCard.tsx
+++ app/components/spotCard/spotCard.tsx
@@ -1,16 +1,42 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import styles from './page.module.css';
 import library from '../../../public/schools/library.jpg';
-import { StudySpot } from '@prisma/client';
+import { StudySpot, Review } from '@prisma/client';
 import Link from 'next/link';
 import Stars from '../stars/stars';
+import { getReviews } from '@/lib/getReviews';
 
 interface spotCardFormProps {
   spotData: StudySpot | null;
 }
 
 const SpotCard: React.FC<spotCardFormProps> = ({ spotData }) => {
+  const [reviewData, setReviewData] = useState<Review[] | null>(null);
+
+  useEffect(() => {
+    const fetchMoreData = async () => {
+      try {
+        const response = await fetch('/api/reviews/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(spotData?.id),
+        });
+        if (response.ok) {
+          const responseData = await response.json();
+          const parsedData = await responseData.reviews;
+          setReviewData(parsedData);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchMoreData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Link className={styles.link} href={`/studyspot/${spotData?.id}`}>
       <div className={styles.spot}>
@@ -26,13 +52,12 @@ const SpotCard: React.FC<spotCardFormProps> = ({ spotData }) => {
 
           <div className={styles.spotOpinion}>
             <p className={styles.spotRating}>
-              {' '}
-              <Stars rating={Math.floor(Math.random() * 5) + 1} />
+              <Stars rating={spotData?.rating ?? 0} />
             </p>
           </div>
-          <p className={styles.spotReviews}>{`${
-            Math.floor(Math.random() * 10) + 1
-          } reviews`}</p>
+          <p
+            className={styles.spotReviews}
+          >{`${reviewData?.length} reviews`}</p>
           <p className={styles.spotAddress}>{spotData?.address}</p>
         </div>
       </div>
